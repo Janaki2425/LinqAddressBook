d9911aee4c35933c49d0940db3769a83393478b4
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-14 15:42:27 +0530

    sort alphabetically

---
+++ Sort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    internal class Sort
+    {
+        public static void SortingAlphabetically(List<AddressDetails> myList)
+        {
+            var orderByResult = from m in myList
+                                orderby m.FirstName
+                                select m;
+
+            foreach (var list in myList)
+            {
+                Console.WriteLine("FirstName:" + list.FirstName + " " + "LastName:" + list.LastName + " " + "Address:" + list.Address + " " + "PhoneNumber:" + list.PhoneNumber + " " + "City:" + list.City + " " + "State:" + list.State + " " + "ZIP:" + list.ZIP + " " + "Email:" + list.Email);
+            }
+        }
+      
+    }
+}
+                
+            
+
+
+        
