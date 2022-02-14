ff9a7fb7415e43cb717dfdbe5913294a859e1953
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-14 15:27:02 +0530

    address details

---
+++ AddressDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    internal class AddressDetails
+    {
+        private string firstname;
+        private string lastname;
+        private string address;
+        private string city;
+        private string phonenumber;
+        private string zip;
+        private string state;
+        private string email;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string ZIP { get; set; }
+        public string Email { get; set; }
+        public AddressDetails(string firstname,string lastname,string address,string city,string phonenumber,string zip,string state,string email)
+        {
+            FirstName= firstname;
+            LastName= lastname;
+            Address= address;
+            City= city;
+            State= state;
+            Email= email;
+            PhoneNumber= phonenumber;
+            ZIP= zip;
+        }
+
+
+
+
+
+    }
+}
