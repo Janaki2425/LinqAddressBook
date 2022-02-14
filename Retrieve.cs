2a6250ffbfaae4a1309be315df7f9b3185591a71
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-14 15:39:13 +0530

    retrieve contact

---
+++ Retrieve.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    internal class Retrieve
+    {
+        public static void RetrieveRecord(List<AddressDetails> myList)
+        {
+            foreach (var address in myList)
+            {
+                if (address.City == "Chennai")
+                {
+
+                    foreach (var list in myList)
+                    {
+                        Console.WriteLine("FirstName:" + list.FirstName + " " + "LastName:" + list.LastName + " " + "Address:" + list.Address + " " + "PhoneNumber:" + list.PhoneNumber + " " + "City:" + list.City + " " + "State:" + list.State + " " + "ZIP:" + list.ZIP + " " + "Email:" + list.Email);
+                    }
+                }
+            }
+        }
+        public static void Count(List<AddressDetails> myList)
+        {
+            foreach (var address in myList)
+            {
+                if (address.City.Length != 0 || address.State.Length != 0)
+                {
+
+                    foreach (var list in myList)
+                    {
+                        Console.WriteLine("FirstName:" + list.FirstName + " " + "LastName:" + list.LastName + " " + "Address:" + list.Address + " " + "PhoneNumber:" + list.PhoneNumber + " " + "City:" + list.City + " " + "State:" + list.State + " " + "ZIP:" + list.ZIP + " " + "Email:" + list.Email);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+                
+            
+    
+
+        
+    
+
